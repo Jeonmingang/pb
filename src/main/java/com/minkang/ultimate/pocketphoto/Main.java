0c7437797c7b242e9dda1bfe19a1a1198373bec8
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-19 02:24:30 +0900

    Update Main.java

--- src/main/java/com/minkang/ultimate/pocketphoto/Main.java
+++ src/main/java/com/minkang/ultimate/pocketphoto/Main.java
@@ -6,7 +6,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
     private static Main inst;
     private boolean pixelmonPresent;
 
@@ -14,22 +13,16 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         inst = this;
         saveDefaultConfig();
-
         pixelmonPresent = classExists("com.pixelmonmod.pixelmon.Pixelmon");
-        if (!pixelmonPresent) {
-            getLogger().warning("Pixelmon not detected. Features require Pixelmon at runtime.");
-        }
-
-        if (getCommand("포켓사진") != null) {
-            getCommand("포켓사진").setExecutor(new PocketPhotoCommand(this));
-        }
+        if (!pixelmonPresent) getLogger().warning("Pixelmon not detected. Features require Pixelmon at runtime.");
+        if (getCommand("포켓사진") != null) getCommand("포켓사진").setExecutor(new PocketPhotoCommand(this));
         Bukkit.getPluginManager().registerEvents(new PhotoItemListener(this), this);
+        getLogger().info("[PocketPhoto] Enabled v" + getDescription().getVersion());
     }
 
     private boolean classExists(String name) {
         try { Class.forName(name); return true; } catch (Throwable t) { return false; }
     }
-
-    public static Main get() { return inst; }
-    public boolean isPixelmonPresent() { return pixelmonPresent; }
+    public static Main get(){ return inst; }
+    public boolean isPixelmonPresent(){ return pixelmonPresent; }
 }
